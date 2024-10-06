5158d5a02a742138c378fcb7d48d64b308203f87
yaminibaskar29 <cs21b056@iittp.ac.in>
2024-10-06 23:13:42 +0530

    sidebarfixed

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,5 +1,4 @@
 "use client";
-"use client";
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'; // Import usePathname
@@ -9,15 +8,10 @@ import { BsInfoSquare, BsEnvelopeAt } from 'react-icons/bs';
 import { FaTshirt, FaRedhat } from 'react-icons/fa';
 
 export default function Sidebar({ show, setter }) {
-   
-    const className = "bg-black w-[250px] transition-[margin-left] ease-in-out duration-500 fixed md:static top-0 bottom-0 left-0 z-40";
-   
-    const appendClass = show ? " ml-0" : " ml-[-250px] md:ml-0";
+    const className = `fixed left-0 top-0 h-screen p-5 bg-black w-[250px] transition-[margin-left] ease-in-out duration-500 z-40 ${show ? "ml-0" : "ml-[-250px] md:ml-0"}`;
 
-   
     const MenuItem = ({ icon, name, route }) => {
         const pathname = usePathname(); 
-
         const colorClass = pathname === route ? "text-white" : "text-white/50 hover:text-white";
 
         return (
@@ -28,7 +22,7 @@ export default function Sidebar({ show, setter }) {
                 }}
                 className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3 border-b-[1px] border-b-white/10 ${colorClass}`}
             >
-                <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                <div className="text-xl flex [&>*]:mx-auto w-[30px] text-purple-500">
                     {icon}
                 </div>
                 <div>{name}</div>
@@ -36,7 +30,6 @@ export default function Sidebar({ show, setter }) {
         );
     };
 
-    
     const ModalOverlay = () => (
         <div
             className={`flex md:hidden fixed top-0 right-0 bottom-0 left-0 bg-black/50 z-30`}
@@ -48,36 +41,16 @@ export default function Sidebar({ show, setter }) {
 
     return (
         <>
-            <div className={`${className}${appendClass}`}>
+            <div className={className}>
                 <div className="flex flex-col">
-                    <MenuItem
-                        name="Home"
-                        route="/"
-                        icon={<SlHome />}
-                    />
-                    <MenuItem
-                        name="Create"
-                        route="/t-shirts"
-                        icon={<FaTshirt />}
-                    />
-                    <MenuItem
-                        name="Tutorials"
-                        route="/hats"
-                        icon={<FaRedhat />}
-                    />
-                    <MenuItem
-                        name="Community"
-                        route="/about"
-                        icon={<BsInfoSquare />}
-                    />
-                    <MenuItem
-                        name="Myfiles"
-                        route="/contact"
-                        icon={<BsEnvelopeAt />}
-                    />
+                    <MenuItem name="Home" route="/" icon={<SlHome />} />
+                    <MenuItem name="Create" route="/t-shirts" icon={<FaTshirt />} />
+                    <MenuItem name="Tutorials" route="/hats" icon={<FaRedhat />} />
+                    <MenuItem name="Community" route="/about" icon={<BsInfoSquare />} />
+                    <MenuItem name="Myfiles" route="/contact" icon={<BsEnvelopeAt />} />
                 </div>
             </div>
-            {show ? <ModalOverlay /> : <></>}
+            {show ? <ModalOverlay /> : null}
         </>
     );
 }
